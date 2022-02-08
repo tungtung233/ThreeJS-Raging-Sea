358a3982b422adce3f2c58c7acdd24d043b1d0b4
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-08 18:22:19 -0500

    Implement fog logic in fragment shader

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -20,4 +20,14 @@ void main()
   vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
 
   gl_FragColor = vec4(color, 1.0);
+
+  #ifdef USE_FOG
+    #ifdef USE_LOGDEPTHBUF_EXT
+        float depth = gl_FragDepthEXT / gl_FragCoord.w;
+    #else
+        float depth = gl_FragCoord.z / gl_FragCoord.w;
+    #endif
+    float fogFactor = smoothstep( fogNear, fogFar, depth );
+    gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );
+  #endif
 }
