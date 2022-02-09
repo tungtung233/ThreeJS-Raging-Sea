e175fe273b75a0d4bdccdbfc00c8066bcdf59e0f
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-09 01:02:06 -0500

    Add lightning pointLight

--- src/script.js
+++ src/script.js
@@ -209,6 +209,10 @@ scene.add(water);
 const ambient = new THREE.AmbientLight(0x555555);
 scene.add(ambient);
 
+const lightning = new THREE.PointLight(0x062d89, 100, 500, 1.7);
+lightning.position.set(200, 45, 100); //just a little in front the cloud
+scene.add(lightning);
+
 /**
  * Sizes
  */
